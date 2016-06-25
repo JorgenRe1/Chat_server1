e6ecd816791365f90b944ab755a58a8cd2609d2e
JorgenRe1 <jorgenreinnes@gmail.com>
2016-06-25 11:26:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 var http = require('http'),
     fs = require('fs');
-	
-var users = {};
-var counter = 0;
+  console.log('start');
+
+//Kjøres bare når en bruker går til siden
 var app = http.createServer(function (request, response) {
-    fs.readFile("chatClient.html", 'utf-8', function (error, data) {
+    fs.readFile("client.html", 'utf-8', function (error, data) {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         response.end();
     });
-}).listen(1337);
+}).listen(1337, 'localhost');
  
 var io = require('socket.io').listen(app);
- 
-io.sockets.on('connection', function(socket) {
-	console.log("User entered!");
-	socket.on("disconnect", function(){
-		console.log("User left");
-	});
-    socket.on('message_to_server', function(data) {
-		if (data["message"] == "new"){
-			//create new ID
-			var newID = (Math.floor(Math.random() * 1000));
-			while (users[newID] != null){
-			   newID = (Math.floor(Math.random() * 1000));	
-			}
-			//console.log("newID: "+newID);
-			io.sockets.emit("message_to_client",{ message: newID});
-		} else {
-			//console.log(data["username"]+": "+data["message"]);
-            io.sockets.emit("message_to_client",{ pID: data["pID"], xPos: data["xPos"], yPos: data["yPos"], angle: data["angle"]});
-		}
+
+var brukere = [];
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  //Når en melding blir sendt til server
+  socket.on('message_to_server', function(data) {
+	  //send til alle tilkoblede brukerne
+        io.sockets.emit("message_to_client",{ message: data["message"] });
     });
+  socket.on('message_to_server3', function (data) {
+	  console.log("ID"+socket.id);
+	  var cid = socket.id;
+	  io.cid.send("message_to_client",{ message: "Hello" });
+  });
+  socket.on('message_to_server2',function(data){
+	  console.log("MSG: "+data["message"]+"ID: "+socket.id);
+      io.to(socket.id).emit('message_to_client',{message:data["message"]});
+  });
+  //Når en bruker logger av
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
