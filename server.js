2a46e3447cd67a1b3f4f2fd91ec4cd55f1dc9368
JorgenRe1 <jorgenreinnes@gmail.com>
2016-06-27 01:06:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,23 +15,25 @@ var io = require('socket.io').listen(app);
 
 var brukere = [];
 var chatter = [];
-
+var cid_fb = [];
 io.on('connection', function(socket){
   console.log('a user connected');
+  
     socket.on('ny_bruker', function(data) {
     	console.log("Ny bruker");
       	var cid = socket.id;
+      	var fb_id = data["fb_id"];
+      	cid_fb[cid] = fb_id; 
     	if(brukere[cid] == null){
          var navn = data["navn"];
-         var fb_id = data["fb_id"];
          console.log("FB: "+fb_id);
-	 brukere[cid] = [];
-	 brukere[cid]["navn"] = navn;
-	 brukere[cid]["fb_id"] = fb_id;
-	 brukere[cid]["last"] = "keine";
-	 brukere[cid]["logg"] = "";
+	 brukere[fb_id] = [];
+	 brukere[fb_id]["navn"] = navn;
+	 brukere[fb_id]["cid"] = cid;
+	 brukere[fb_id]["last"] = "keine";
+	 brukere[fb_id]["logg"] = "";
     	}
-    	 brukere[cid]["status"] = true;	
+    	 brukere[fb_id]["status"] = true;	
     	
     });
   //Når en melding blir sendt til server
@@ -42,21 +44,18 @@ io.on('connection', function(socket){
    //Hente alle brukere som er logget paa
   socket.on('hent_brukere', function(data) {
 	  var bruker_liste = "";
+	  //Hente alle facebook idean
 	  var bruker_ider = Object.keys(brukere);
-	  console.log(bruker_ider);
 	  for (var nr = 0; nr < bruker_ider.length+1; nr++){
 	  	if (bruker_ider[nr] != socket.id && bruker_ider[nr] != null && brukere[bruker_ider[nr]]["status"]){
-	  		console.log(nr+" : "+bruker_ider[nr]);
 	  		if (bruker_liste == "") {
 	  			var bruker_navn = brukere[bruker_ider[nr]]["navn"];
-	  			var fb_id = brukere[bruker_ider[nr]]["fb_id"];
 	  			bruker_liste = "<button";
-	  			bruker_liste +=" onclick='chat_med(\""+bruker_ider[nr]+"\", \""+bruker_navn+"\", \""+fb_id+"\")' class='chat_med_btn'>"+bruker_navn+"</button>";
+	  			bruker_liste +=" onclick='chat_med(\""+bruker_ider[nr]+"\", \""+bruker_navn+"\", \""+bruker_ider[nr]+"\")' class='chat_med_btn'>"+bruker_navn+"</button>";
 	  		} else if (bruker_ider[nr] != null){
 	  			var bruker_navn = brukere[bruker_ider[nr]]["navn"];
-	  			var fb_id = brukere[bruker_ider[nr]]["fb_id"];
 	  			bruker_liste += "<br><button";
-	  			bruker_liste +=" onclick='chat_med(\""+bruker_ider[nr]+"\", \""+bruker_navn+"\", \""+fb_id+"\")' class='chat_med_btn'>"+bruker_navn+"</button>";
+	  			bruker_liste +=" onclick='chat_med(\""+bruker_ider[nr]+"\", \""+bruker_navn+"\", \""+bruker_ider[nr]+"\")' class='chat_med_btn'>"+bruker_navn+"</button>";
 	  		}
 	  	}
 	  }
@@ -65,44 +64,42 @@ io.on('connection', function(socket){
     //Når bruker chatter:
   socket.on('message_to_server', function (data) {
 	  var cid = socket.id;
+	  var fb_id = cid_fb[cid];
 	  var navn = data["navn"];
-	  var logg = brukere[cid]["logg"];
 	  console.log("ID: "+cid);
 	  if (brukere[cid] == null){
-	  	console.log("Ny bruker");
-	  	brukere[cid] = [];
-	  	brukere[cid]["last"] = cid;
-	  	brukere[cid]["navn"] = navn;
-	  	brukere[cid]["logg"] = "<span style='font-weight: bold; border-bottom: solid black;'>"+navn+"</span><br>&nbsp"+data["message"];
-	        brukere[cid]["status"] = true;
+              var msg_t = "ikke_registrert";
 	  } else {
-	  	console.log("Naa: "+cid+" Last: "+brukere[cid]["last"]);
+	  	var logg = brukere[fb_id]["logg"];
+	  	console.log("Naa: "+fb_id+" Last: "+brukere[fb_id]["last"]);
 	  	console.log("MSG: "+data["message"]);
-	  	if (brukere[cid]["last"] == cid){
-	  	    brukere[cid]["logg"] += "<br>&nbsp"+data["message"];	
+	  	if (brukere[fb_id]["last"] == fb_id){
+	  	    brukere[fb_id]["logg"] += "<br>&nbsp"+data["message"];	
 	  	} else {
-	  	   if (logg != "") brukere[cid]["logg"] += "<br>";
-	  	   brukere[cid]["logg"] += "<span style='font-weight: bold; border-bottom: solid black;'>"+brukere[cid]["navn"]+"</span><br>&nbsp"+data["message"];
+	  	   if (logg != "") brukere[fb_id]["logg"] += "<br>";
+	  	   brukere[fb_id]["logg"] += "<span style='font-weight: bold; border-bottom: solid black;'>"+brukere[cid]["navn"]+"</span><br>&nbsp"+data["message"];
 	  	}
+	  	var msg_t = brukere[fb_id]["logg"];
 	  }
-	  brukere[cid]["last"] = cid;
+	  brukere[fb_id]["last"] = fb_id;
 	  console.log("Sender melding til: "+cid);
-	  io.to(cid).emit('message_to_client',{message: brukere[cid]["logg"], from: "self"});
+	  io.to(cid).emit('message_to_client',{message: msg_t, from: "self"});
 	  //Send saa til admin som kan da bli notifisert om at det chattes
 	  //io.sockets.emit("notify_admin",{ message: cid });
   });
   
   //når admin chatter så må chatt logg objectet sendes til admin og bruker som hjelpes
   socket.on('admin_to_user',function(data){
-	  var user_id = data["user_id"];
+  	  var fb_id = data["user_id"];
+	  var user_id = brukere[fb_id]["cid"];
 	  var admin_id = socket.id;
 	 
-	  if (brukere[user_id]["last"] == admin_id){
-	        brukere[user_id]["logg"] += "<br>&nbsp"+data["message"];	
+	  if (brukere[fb_id]["last"] == admin_id){
+	        brukere[fb_id]["logg"] += "<br>&nbsp"+data["message"];	
 	  } else {
-	  	brukere[user_id]["logg"] += "<br><span style='font-weight: bold; border-bottom: solid black;'>"+brukere[admin_id]["navn"]+"</span><br>&nbsp"+data["message"];
+	  	brukere[fb_id]["logg"] += "<br><span style='font-weight: bold; border-bottom: solid black;'>"+brukere[admin_id]["navn"]+"</span><br>&nbsp"+data["message"];
 	  }
-	  brukere[user_id]["last"] = admin_id;
+	  brukere[fb_id]["last"] = admin_id;
 	  var logg = brukere[user_id]["logg"];
           io.to(admin_id).emit('message_to_client',{message: logg, from: "self"});
           io.to(user_id).emit('message_to_client',{message: logg, from: brukere[admin_id]["navn"]});
@@ -115,6 +112,7 @@ io.on('connection', function(socket){
   //Når en bruker logger av
   socket.on('disconnect', function(){
     console.log('user disconnected. ID: '+socket.id);
-    if (brukere[socket.id] != null ) brukere[socket.id]["status"] = false;	
+    var user_id = cid_fb[socket.id];
+    if (brukere[user_id] != null ) brukere[user_id]["status"] = false;	
   });
 });
