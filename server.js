444371a5c6fd8f3cc6fbfdfc48d7baea4f87872f
JorgenRe1 <jorgenreinnes@gmail.com>
2016-06-27 11:28:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ var chatter = [];
 var cid_fb = [];
 io.on('connection', function(socket){
   console.log('a user connected');
-  //Se om bruker har allerede vært logget på. I såfall logg på
-    socket.on('message_all', function(data) {
-	  if (brukere[data["fb_id"]] != null]) brukere[data["fb_id"]]["status"] = true;
-    });
     socket.on('ny_bruker', function(data) {
     	console.log("Ny bruker");
       	var cid = socket.id;
